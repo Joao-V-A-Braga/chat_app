50bc2cd712b1b2d4bb873abed6a98fdd1b2048f5
jxvx <jvab@aluno.ifnmg.edu.br>
2023-11-02 11:41:55 -0300

    :lipstick: Style #layout -> add logo, favicon and name to app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Chat App</title>
+
+        <link rel="icon" href="{{Vite::asset('resources/images/favicon.ico')}}" />
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
